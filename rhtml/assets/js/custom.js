c6b2327ba5dfc92329e10845398f0e14b8d5ce38
Ram Chandra <chaurasia4u@gmail.com>
2023-06-29 22:56:59 +0530

    update ui changes

--- rhtml/assets/js/custom.js
+++ rhtml/assets/js/custom.js
@@ -1,18 +1,5 @@
 /* JS Document */
 
-/******************************
-
-[Table of Contents]
-
-1. Vars and Inits
-2. Set Header
-3. Init Menu
-4. Init Home Slider
-5. Init News Slider
-6. Init Milestones
-
-
-******************************/
 AOS.init();
 
 $(document).ready(function () {
@@ -24,10 +11,7 @@ $(document).ready(function () {
 
 	setHeader();
 	initMenu();
-	initHomeSlider();
-	//initNewsSlider();
-	//initMilestones();
-
+	
 	$(window).on('resize', function () {
 		setHeader();
 
@@ -47,7 +31,7 @@ $(document).ready(function () {
 
 		if ($(window).scrollTop() > 91) {
 			header.addClass('scrolled');			
-			$('.header.scrolled .logo_container img').attr('src','assets/images/logo_black.png');		 
+			$('.header.scrolled .logo_container img').attr('src','assets/images/logo_black.png'); 
 			
 		}
 		else {
@@ -74,85 +58,55 @@ $(document).ready(function () {
 		}
 	}
 
-	/* 	4. Init Home Slider	*/
-
-	function initHomeSlider() {
-		if ($('.img_slider').length) {
-			var homeSlider = $('.img_slider');
-			homeSlider.owlCarousel(
-				{
-					items: 3,
-					autoplay:1,					
-					loop: true,
-					nav: false,
-					dots: true,
-					smartSpeed: 300,
-					responsive: {
-						0: {
-							items: 1,
-							
-						},
-						600: {
-							items: 2,
-							
-						},
-						1000: {
-							items: 3,
-							
-						}
-					}
-				});
+	
 
 
-			$('.logolist').owlCarousel({
-				loop: true,
-				margin: 30,
-				nav: true,
-				autoplay:true,
-				dots: false,
-				responsive: {
-					0: {
-						items: 3
-					},
-					600: {
-						items: 6
-					},
-					1000: {
-						items: 9
-					}
-				}
-			})
-			$(".owl-prev").html('<i class="fa fa-caret-left" aria-hidden="true"></i>');
-			$(".owl-next").html('<i class="fa fa-caret-right" aria-hidden="true"></i>');
+});
 
+$('.logolist').owlCarousel({
+	loop: true,
+	margin: 30,
+	nav: true,
+	autoplay:true,
+	dots: false,
+	responsive: {
+		0: {
+			items: 3
+		},
+		600: {
+			items: 6
+		},
+		1000: {
+			items: 9
+		}
+	}
+})
+$(".owl-prev").html('<i class="fa fa-caret-left" aria-hidden="true"></i>');
+$(".owl-next").html('<i class="fa fa-caret-right" aria-hidden="true"></i>');
 
-			$('.partnerSlider').owlCarousel({
-				loop: true,
-				margin: 10,
-				dots: false,
-				smartSpeed: 300,
-				autoplay:true,
-				responsiveClass: true,
-				responsive: {
-					0: {
-						items: 2,
-						nav: true
-					},
-					600: {
-						items: 3,
-						nav: false
-					},
-					1000: {
-						items: 4,
-						nav: true,
-						loop: false
-					}
-				}
-
-			})
-			$(".owl-prev").html('<i class="fa fa-caret-left" aria-hidden="true"></i>');
-			$(".owl-next").html('<i class="fa fa-caret-right" aria-hidden="true"></i>');
+$('.partnerSlider').owlCarousel({
+	loop: true,
+	margin: 10,
+	dots: false,
+	smartSpeed: 300,
+	autoplay:true,
+	responsiveClass: true,
+	responsive: {
+		0: {
+			items: 2,
+			nav: true
+		},
+		600: {
+			items: 3,
+			nav: false
+		},
+		1000: {
+			items: 4,
+			nav: true,
+			loop: false
 		}
 	}
 
-});
+})
+$(".owl-prev").html('<i class="fa fa-caret-left" aria-hidden="true"></i>');
+$(".owl-next").html('<i class="fa fa-caret-right" aria-hidden="true"></i>');
