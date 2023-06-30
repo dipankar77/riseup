9ea3525fda16a14038054f0c362c7a4363825922
Ram Chandra <chaurasia4u@gmail.com>
2023-06-30 22:01:32 +0530

    update hpme page ui

--- rhtml/assets/js/custom.js
+++ rhtml/assets/js/custom.js
@@ -97,11 +97,11 @@ $('.partnerSlider').owlCarousel({
 			nav: true
 		},
 		600: {
-			items: 3,
+			items: 2,
 			nav: false
 		},
 		1000: {
-			items: 4,
+			items: 3,
 			nav: true,
 			loop: false
 		}
